dbc3cbdd1cc3e66844d63712caa4aa46fbd0a0f2
NguyenThiChung <chungnguyen.130902@gmail.com>
2023-06-30 23:34:33 +0700

    up

--- constants/dbSettings.js
+++ constants/dbSettings.js
@@ -1,11 +1,11 @@
 module.exports = {
   // CONNECTION_STRING: 'mongodb+srv://developer:147258369@cluster0.w8qpr.mongodb.net/test',
   // DATABASE_NAME: 'test',
-  CONNECTION_STRING: "mongodb://127.0.0.1:27017/batch-nodejs-database",
-  DATABASE_NAME: "batch-nodejs-database",
+  // CONNECTION_STRING: "mongodb://127.0.0.1:27017/batch-nodejs-database",
+  // DATABASE_NAME: "batch-nodejs-database",
   // CONNECTION_STRING: 'mongodb+srv://new-user-1:123123123@cluster0.mrci4cv.mongodb.net/test',
   // DATABASE_NAME: 'training-database',
 
-  // CONNECTION_STRING: 'mongodb+srv://trinhphuongdesigner:12345678p@cluster0.mrci4cv.mongodb.net/test',
-  // DATABASE_NAME: 'test',
+  CONNECTION_STRING: 'mongodb+srv://chungnguyen130902:Chung130902@cluster0.ws8o9zg.mongodb.net/batch-nodejs-database',
+  DATABASE_NAME: 'batch-nodejs-database',
 };
